dbcd303dddcfd734a0cadfd3aa353e5c457fb6c3
Jay <jay0828@vt.edu>
2017-12-08 18:31:49 -0500

    added to rmq_params

--- rmq_params.py
+++ rmq_params.py
@@ -2,7 +2,7 @@ rmq_params = {
     "vhost": "test",
     "username": "pi",
     "password": "raspberry",
-    "exchanges": {"apptoserver", "servertostorage"},
-    "queuesGame": {"player1"},
+    "exchanges": {"apptoserver", "servertostorage", "servertogame", "gametoserver"},
+    "queuesGame": {"player1", "player2", "board"},
     "queuesStorage": {"store"}
 }
