9f714f4215118ef2069d37bc2c0e9bca3dddb58c
Chris Boado <boadoct@vt.edu>
2017-12-24 16:31:08 -0500

    starting fresh

--- rmq_params.py
+++ rmq_params.py
@@ -1,9 +1,9 @@
 rmq_params = {
-    "vhost": "test",
-    "username": "pi",
-    "password": "raspberry",
-    "bridgeip": "172.29.119.171",
-    "exchanges": {"apptoserver", "servertostorage"},
-    "queuesGame": {"player1", "player2", "server"},
-    "queuesStorage": {"store"}
+    "vhost": "",
+    "username": "",
+    "password": "",
+    "bridgeip": "",
+    "exchanges": {""},
+    "queuesGame": {""},
+    "queuesStorage": {""}
 }
