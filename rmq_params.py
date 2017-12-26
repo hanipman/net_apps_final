f8d1c2ac0f6f11fa5ce59d35f914fecdb15f0926
Chris Boado <boadoct@vt.edu>
2017-12-26 00:53:47 -0500

    Fixed gitignore

--- rmq_params.py
+++
@@ -1,7 +0,0 @@
-rmq_params = {
-    "vhost": "test",
-    "username": "pi",
-    "password": "raspberry",
-    "bridgeip": "raspberrypi32.local",
-    "exchanges": {"apptoserver", "servertodatabase"}
-}
