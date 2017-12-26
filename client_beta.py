19af132938c019a6fe4e03ecd914d1bf8df1a7df
Chris Boado <boadoct@vt.edu>
2017-12-26 17:05:13 -0500

    Added main menu account function

--- client_beta.py
+++ client_beta.py
@@ -4,52 +4,110 @@ import json
 import getopt
 import sys
 import os
-import curses
 from rmq_params import rmq_params
-from enum import Enum
 
 cred = pika.PlainCredentials(rmq_params['username'], rmq_params['password'])
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=rmq_params['bridgeip'], virtual_host=rmq_params['vhost'], credentials=cred))
 channel = connection.channel();
+channel.exchange_declare(exchange='apptoserver', exchange_type='direct')
 print("Connected to vhost'" + rmq_params["vhost"] + "' on RMQ server at 'localhost' as user'" + rmq_params["username"] + "'")
 
 state = "mainMenu"
 loggedIn = False
 user = ""
-selector = 0
-mainMenu = ["Login", "Play", "Settings"]
 
-def mainMenu(stdscr):
-    attributes = {}
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    attributes['highlighted'] = curses.color_pair(2)
+def clearScreen():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
-    c = 0
-    option = 0
-    while c != 10:
-        stdscr.erase()
-        stdscr.addstr("What is your class?\n", curses.A_UNDERLINE)
-        for i in range(len(mainMenu)):
-            if i == option:
-                attr = attributesw['highlighted']
-            else:
-                attr = attributes['normal']
-            stdscr.addstr("{0}. ".format(i + 1))
-            stdscr.addstr(mainMenu[i] + '\n', attr)
-        c = stdscr.getch()
-        if c == curses.KEY_UP and option > 0:
-            option -= 1
-        elif c == curses.KEY_DOWN and option < len(mainMenu) - 1:
-            option += 1
-    stdscr.addstr("You chose {0}".format(mainMenu[option]))
-    stdscr.getch()
+def logInPrompt():
+    global loggedIn
+    global user
+    clearScreen()
+    print("Login")
+    print("To go back type 'back'")
+    user = input("Username: ")
+    if user == "back":
+        return None
+    pwd = input("Password: ")
+    if pwd == "back":
+        return None
+    cred = json.dumps(["Login", {"Username": user, "Password": pwd}])
+    channel.basic_publish(exchange="apptoserver", routing_key="server", body=cred)
+    loggedIn = True
+
+def createAccountPrompt():
+    global loggedIn
+    global user
+    clearScreen()
+    print("Create Account")
+    print("To go back type 'back'")
+    user = input("Username: ")
+    if user == "back":
+        return None
+    pwd = input("Password: ")
+    if pwd == "back":
+        return None
+    cred = json.dumps(["Create Account", {"Username": user, "Password": pwd}])
+    channel.basic_publish(exchange="apptoserver", routing_key="server", body=cred)
+    loggedIn = True
+
+def logoutPrompt():
+    global loggedIn
+    clearScreen()
+    while True:
+        print("Are you sure you wish to logout?")
+        text = input("Y/N: ")
+        if text == "Y":
+            loggedIn = False
+            break;
+        elif text == "N":
+            break
+        else: # text != "Y" or text != "N"
+            print("Invalid choice: " + text)
 
 while True:
-    clear = lambda: os.system('cls')
-    clear()
+    clearScreen()
     if state == "mainMenu":
-        curses.wrapper(mainMenu)
+        if not loggedIn:
+            print("Login")
+            print("Create Account")
+            text = input("Choose: ")
+            if text == "Login":
+                logInPrompt()
+                continue
+            elif text == "Create Account":
+                createAccountPrompt()
+                continue
+            else: # invalid text
+                print("Invalid Choice: " + text)
+                continue
+        else: #loggedin
+            print("User: " + user)
+            print("Logout")
+            print("Play")
+            print("Settings")
+            text = input("Choose: ")
+            if text == "Logout":
+                logoutPrompt()
+                continue;
+            elif text == "Play":
+                state = "playGame"
+                continue
+            elif text == "Settings":
+                state = "Settings"
+                continue
+            else:
+                print("Invalid Choice: " + text)
+                continue
     elif state == "playGame":
-        continue;
-    else: # state == "settings"
-        continue;
+        print("Entered playGame state")
+        text = input("Type anything to go back to main menu: ")
+        state = "mainMenu"
+
+    else: # state == "Settings"
+        print("Entered Settings state")
+        text = input("Type anything to go back to main menu: ")
+        state = "mainMenu"
